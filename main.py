3ef0e06a6a021c1aa96605f4f115a020310b99b6
WalDragnir <akgk530@gmail.com>
2023-01-17 03:30:20 +0200

    Starting changes

---
+++ main.py
@@ -0,0 +1,15 @@
+def row_weights(row: list) -> list:
+    rl = [0, 0]
+    i = 1
+    for num in row:
+        if i == 1:
+            rl[0] += num
+            i += 1
+        if i == 2:
+            rl[1] += num
+            i -= 1
+    return rl
+
+
+row_weights([8, 5, 9, 3])
+print(row_weights([1, 2, 4, 2, 3]))
